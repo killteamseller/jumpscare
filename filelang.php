c8bd533e063b0c8e6f184d0bd4a7a44e2c8fab26
killteamseller <74624892+killteamseller@users.noreply.github.com>
2021-01-20 13:23:23 +0100

    Update and rename index.php to filelang.php

---
+++ filelang.php
@@ -0,0 +1,3 @@
+
+(file creato per risolvere bug con altri siti nella build) 
+puoi  cancellare tranquillamente il file se stai sviluppando con il tuo server
--- index.php
+++
@@ -1,4 +0,0 @@
-Index del tuo sito
-(file creato per risolvere bug con altri siti nella build) 
-
-sostituisci il file con il tuo index
